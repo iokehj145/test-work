472bf2a64d898251c1443335d0004a8e65b5a95f
iokehj <iiiokehj@gmail.com>
2024-10-18 10:39:39 +0300

    Added themes

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const InputHeader2 = document.getElementById("input2");
 const Button = document.getElementById("button");
 const list = document.getElementById("list");
 const array_check = [""," "];
+function showSelectedTime() {
+    const timeInput = document.getElementById("timePicker");
+    const selectedTime = timeInput.value;
+    
+    // Display the selected time
+    const display = document.getElementById("displayTime");
+    if (selectedTime) {
+      display.textContent = `Selected time is: ${selectedTime}`;
+    } else {
+      display.textContent = "Please select a time.";
+    }
+  }
 // creat a task
-
 const CreateTask =()=> {
     const check = array_check.every(element => element !== InputBox.value) // check if input contains at least one array element
     if(check){
@@ -36,6 +47,28 @@ const SaveData =()=>
     localStorage.setItem("data", list.innerHTML);
 
 // get data from local storage
-const ListToFill =()=> 
-    list.innerHTML = localStorage.getItem("data");
-ListToFill();
+const UpdatePage =()=> {
+    const SavedData = localStorage.getItem("data");
+    if (SavedData) list.innerHTML = SavedData;
+    const SavedTheme = localStorage.getItem("theme");
+    if(SavedTheme) ClickTheme(localStorage.getItem("theme"));
+}
+// toggle menu
+const ActiveteNav = () => document.getElementsByClassName('All_theme')[0].classList.toggle('active');
+// switch theme
+const ClickTheme = (theme) => {
+    const body = document.body;
+    const themes = {
+        w: "linear-gradient(50deg, #fff 50%, #999 100%)",
+        b: "linear-gradient(135deg, #5d7275 10%, #a0dcd8)",
+        g: "linear-gradient(90deg, rgba(83,210,110,1) 38%, rgba(94,181,15,1) 69%)",
+        y: "linear-gradient(90deg, rgba(249,186,90,1) 37%, rgba(211,212,40,1) 69%)",
+    };
+    if (themes[theme]) {
+        body.style.background = themes[theme];
+        localStorage.setItem("theme", theme);
+    } else {
+        console.error("Invalid theme:", theme);
+    }
+};
+UpdatePage();
